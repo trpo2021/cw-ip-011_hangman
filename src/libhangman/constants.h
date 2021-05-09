0fcd5b60a608f32cf0acef8aab5ff5469a1fd322
Roman Kargin <74534183+Kr0sen@users.noreply.github.com>
2021-05-09 15:33:50 +0700

    Reduce bodyparts.c

--- src/libhangman/constants.h
+++ src/libhangman/constants.h
@@ -25,15 +25,48 @@ static const int ERROR_RENDER_NOT_INIT = 4;
 static const int START_COMMAND = 0;
 static const int CLOSE_COMMAND = 999;
 
+/**
+ * Условно делим окно на 16*10 (VIRTUAL_RES_Y*VIRTUAL_RES_X)
+ * частей, по ним строим виселицу и части тела.
+ */
 static const int VIRTUAL_RES_X = 10;
 static const int VIRTUAL_RES_Y = 16;
+
 static const int GIB_RESIZE_LESS = 3;
 static const int GIB_RESIZE_MORE = 2;
+
+static const int POST_X1 = WINDOW_WIDTH / VIRTUAL_RES_X;
+static const int POST_Y1 = WINDOW_HEIGHT / (VIRTUAL_RES_Y / 2);
+static const int POST_W
+        = GIB_RESIZE_MORE * WINDOW_WIDTH / (VIRTUAL_RES_X * GIB_RESIZE_LESS);
+static const int POST_H = 7 * WINDOW_HEIGHT / (VIRTUAL_RES_Y / 2);
+static const int POST_X2 = POST_X1 + POST_W;
+static const int POST_Y2 = POST_Y1 + POST_H;
+
+static const int PLANK_X1 = POST_X2;
+static const int PLANK_Y1 = POST_Y1;
+static const int PLANK_W = 3 * WINDOW_WIDTH / (VIRTUAL_RES_X / 2)
+        + (WINDOW_WIDTH / VIRTUAL_RES_X - POST_W);
+static const int PLANK_H
+        = GIB_RESIZE_MORE * WINDOW_HEIGHT / (VIRTUAL_RES_Y * GIB_RESIZE_LESS);
+static const int PLANK_X2 = PLANK_X1 + PLANK_W;
+static const int PLANK_Y2 = PLANK_Y1 + PLANK_H;
+
+static const int HEAD_X
+        = PLANK_X1 + GIB_RESIZE_MORE * PLANK_W / GIB_RESIZE_LESS;
+static const int HEAD_Y = PLANK_Y2 + 4 * WINDOW_HEIGHT / VIRTUAL_RES_Y;
+static const int HEAD_RAD = WINDOW_HEIGHT / VIRTUAL_RES_Y;
+
+static const int BODY_X = HEAD_X;
+static const int BODY_RAD_X = GIB_RESIZE_MORE * HEAD_RAD / GIB_RESIZE_LESS;
+static const int BODY_RAD_Y = GIB_RESIZE_MORE * HEAD_RAD;
+static const int BODY_Y = HEAD_Y + HEAD_RAD + BODY_RAD_Y;
 
 static const int RED = 255;
 static const int GREEN = 255;
 static const int BLUE = 255;
 static const int ALPHA = 255;
+
 static const int RED_BACK = 0;
 static const int GREEN_BACK = 0;
 static const int BLUE_BACK = 0;
